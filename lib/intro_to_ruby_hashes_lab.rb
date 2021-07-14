c8baa37fa3d8de02277490852762b5bb7fa48f76
Brian Smith <brian.bubbam@gmail.com>
2021-07-14 13:47:23 -0500

    commit tree 27f9c8e1c010aaeb74f6208da6ed81e0fe8572bc

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,13 +1,19 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  { key => value }
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  hash[key]
+
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  if hash[key] # this is saying if the this hash and key exist run the first if statment if not ren the else statment
+    hash[key] = hash[key] + 1 # setting the hash and key equal to the hash and key plus one
+  else
+    hash[key] = 1 # creaing the hash and key and setting it equal to one
+  end
+  hash
 end
